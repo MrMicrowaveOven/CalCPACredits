b68aba6e804023087dfbca3d79054d21d81e347e
Benjamin Zagorski <benj@trustsherpa.com>
2017-03-21 10:26:10 -0700

    Moved juggernaut test to top of test page

--- calculations.js
+++ calculations.js
@@ -1,3 +1,31 @@
+function calcCredits(classLength, numHits, classType) {
+  var maxHits = classLength * 4;
+  var perfectNumber = classLength * 3;
+  var grade = numHits * 1.0 / perfectNumber;
+  if (grade > 1.0) {
+    grade = 1.0
+  }
+  if (classType == "allOrNothing" || (classLength == 1 && classType == "roundDown")) {
+    if (grade >= 1) {
+      return classLength;
+    } else {
+      return 0;
+    }
+  }
+
+  if (classType == "fiftyMinuteHours") {
+    classLength = classLength * 50.0/60;
+    classLength = Math.floor(classLength * 2)/2;
+    classType = "roundDown"
+  }
+
+  if (classType == "roundDown") {
+    var proRated = grade * classLength;
+    var numCredits = Math.floor(proRated * 2)/2;
+    return numCredits;
+  }
+}
+
 function updateHits() {
   var classLength = document.getElementById("classLength").value;
   var maxHits = classLength * 4;
@@ -28,33 +56,6 @@ function classType() {
   return document.getElementById("classType").value;
 }
 
-function calcCredits(classLength, numHits, classType) {
-  var maxHits = classLength * 4;
-  var perfectNumber = classLength * 3;
-  var grade = numHits * 1.0 / perfectNumber;
-  if (grade > 1.0) {
-    grade = 1.0
-  }
-  if (classType == "allOrNothing" || (classLength == 1 && classType == "roundDown")) {
-    if (grade >= 1) {
-      return classLength;
-    } else {
-      return 0;
-    }
-  }
-
-  if (classType == "fiftyMinuteHours") {
-    classLength = classLength * 50.0/60;
-    classLength = Math.floor(classLength * 2)/2;
-    classType = "roundDown"
-  }
-
-  if (classType == "roundDown") {
-    var proRated = grade * classLength;
-    var numCredits = Math.floor(proRated * 2)/2;
-    return numCredits;
-  }
-}
 
 function showCredits() {
   var numCredits = calcCredits(classLength(), numHits(), classType());
@@ -62,8 +63,8 @@ function showCredits() {
   document.getElementById("response").innerHTML = displayString;
 }
 
-// module.exports = {
-//   calcCredits: function(classLength, numHits, classType) {
-//     return calcCredits(classLength, numHits, classType);
-//   }
-// };
+module.exports = {
+  calcCredits: function(classLength, numHits, classType) {
+    return calcCredits(classLength, numHits, classType);
+  }
+};
--- test/request_test.js
+++ test/request_test.js
@@ -1,29 +1,12 @@
-var request = require('../request.js');
+var calculations = require('../calculations.js');
 var chai = require('chai');
 var expect = chai.expect;
 var solution = require('../solution.js')
-// describe("calcCredits(classLength, numHits, classType)", function() {
-//   describe("allOrNothing", function() {
-//     it("passes a 10/12", function() {
-//       expect(request.calcCredits(3, 10, "allOrNothing")).to.equal(3)
-//     })
-//     it("passes a 9/12", function() {
-//       expect(request.calcCredits(3, 9, "allOrNothing")).to.equal(3)
-//     })
-//     it("fails a 8/12", function() {
-//       expect(request.calcCredits(3, 8, "allOrNothing")).to.equal(0)
-//     })
-//   })
-//   describe("roundDown", function() {
-//     it("gives an 11/28 a 3.5", function() {
-//       expect(request.calcCredits(7, 11, "roundDown")).to.equal(3.5)
-//     })
-//   })
-// })
+
 function runTest(classLength, numHits, classType, result) {
   describe("auto " + classType, function() {
     it("gives a " + numHits + "/" + (classLength * 4) + " a " + result + ", " + classLength + " hour class", function() {
-      expect(request.calcCredits(classLength, numHits, classType)).to.equal(result)
+      expect(calculations.calcCredits(classLength, numHits, classType)).to.equal(result)
     })
   })
 }
@@ -46,3 +29,23 @@ classLengths.forEach(function(classLength) {
 })
 
 // runTest(7, 11, "roundDown", 3.5);
+
+
+// describe("calcCredits(classLength, numHits, classType)", function() {
+//   describe("allOrNothing", function() {
+//     it("passes a 10/12", function() {
+//       expect(calculations.calcCredits(3, 10, "allOrNothing")).to.equal(3)
+//     })
+//     it("passes a 9/12", function() {
+//       expect(calculations.calcCredits(3, 9, "allOrNothing")).to.equal(3)
+//     })
+//     it("fails a 8/12", function() {
+//       expect(calculations.calcCredits(3, 8, "allOrNothing")).to.equal(0)
+//     })
+//   })
+//   describe("roundDown", function() {
+//     it("gives an 11/28 a 3.5", function() {
+//       expect(calculations.calcCredits(7, 11, "roundDown")).to.equal(3.5)
+//     })
+//   })
+// })
