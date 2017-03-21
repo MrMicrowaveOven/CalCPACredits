c65b078484b3092a76db41eb7aa0790c8d744fe2
Benjamin Zagorski <benj@trustsherpa.com>
2017-03-20 19:08:34 -0700

    Algorithm now seems to work, juggernaut test object now complete for allOrNothing

--- request.js
+++ request.js
@@ -1,128 +1,57 @@
-function search() {
-  document.getElementById("timesChecked").value = 0;
-  validateInput();
-}
-
-function getInput() {
-  var parameters = document.getElementById("parameters").value.toLowerCase();
-  parameters = parameters.split(" ").join("+");
-  var keyword = document.getElementById("keyword").value.toLowerCase();
-  return [parameters, keyword];
-}
-
-
-function validateInput() {
-  var inputs = getInput();
-  var parameters = inputs[0];
-  var keyword =  inputs[1];
-  if (keyword.length === 0 || parameters.length === 0 ) {
-    document.getElementById("response").innerHTML
-      = "Must have search terms and url.";
-  } else {
-    disableFields();
-    requestForNthPage(keyword, parameters);
-  }
-}
-
-function getTimesChecked() {
-  return document.getElementById("timesChecked");
-}
-
-function disableFields() {
-  document.getElementById("keyword").disabled = "true";
-  document.getElementById("parameters").disabled = "true";
-}
-
-function requestForNthPage(keyword, searchParameters) {
-  var timesChecked = getTimesChecked();
-  timesChecked.value = parseInt(timesChecked.value) + 1;
-  if (timesChecked.value > 10) {
-    document.getElementById("response").innerHTML =
-      "Beyond the 10th page.  Sorry, that's as far as I can go.";
-    return;
-  }
-  var links = [];
-  $.ajax({
-    type: "GET",
-    url: "https://www.googleapis.com/customsearch/v1",
-    data: {
-        key: "AIzaSyD7nKgEWQCBHw1uGJOEvXN-ae4cMeOMXuE",
-        cx: "012667598935678745968:ixjc-b6xzre",
-        fields: "items(link),queries",
-        q: searchParameters,
-        start: (timesChecked.value - 1) * 10 + 1
-    },
-    success: function (res) {
-        res.items.forEach(function (item) {
-          links.push(item.link);
-        });
-        checkLinks(links, keyword, searchParameters);
-    },
-    error: function (xhr, status, error) {
-      if (xhr.status === 403) {
-        document.getElementById("response").innerHTML
-          = "Sorry, ran out of pings for today :(";
-      } else {
-        document.getElementById("response").innerHTML = "Error #" + xhr.status;
-      }
-    }
-  });
-}
-
-function checkLinks(links, keyword, searchParameters) {
-  var timesChecked = getTimesChecked().value;
-  var found = false;
-  links.forEach(function (link, ind) {
-    if(found === false){
-      var numCheck = (timesChecked - 1) * 10 + 1 + ind;
-      // console.log("Checking " + numCheck
-        // + " for " + keyword.toLowerCase());
-      // console.log(link.toLowerCase());
-      if (link.toLowerCase().indexOf(keyword) !== -1) {
-        var linkToSite = document.createElement("a");
-        linkToSite.href = link;
-        linkToSite.innerHTML = 'here';
-        linkToSite.target = "_blank";
-        document.getElementById("response").innerHTML =
-          "'" + keyword + "' is result "
-          + numCheck + " when searching '" + searchParameters + "'."
-          + "  Click ";
-        document.getElementById("response").appendChild(linkToSite);
-        document.getElementById("response2").innerHTML = "to visit the site!";
-        document.getElementById("keepGoing").style.visibility = "hidden";
-        document.getElementById("keyword").disabled = "";
-        document.getElementById("parameters").disabled = "";
-        found = true;
-      }
-    }
-  });
-  if(found === false){
-    askForNextStep(keyword, searchParameters);
-  }
-}
-
-function askForNextStep(keyword, parameters) {
-  var timesChecked = getTimesChecked().value;
-  var first
-    = (parseInt(timesChecked) - 1) * 10 + 1;
-  var last = first + 9;
-  document.getElementById("response").innerHTML =
-    "I've checked page " + timesChecked + ", which contains results " +
-    first + " through " + last + ".  Shall I keep looking?";
-  document.getElementById("keepGoing").style.visibility = "visible";
-  document.getElementById("search").disabled = "true";
-}
-
-function keepGoing() {
-  validateInput();
-}
-
 function reset() {
-  document.getElementById("keepGoing").style.visibility = "hidden";
-  document.getElementById("keyword").disabled = "";
-  document.getElementById("parameters").disabled = "";
-  document.getElementById("search").disabled = "";
-  document.getElementById("response").innerHTML = "";
-  document.getElementById("response2").innerHTML = "";
-  document.getElementById("timesChecked").value = 0;
+  document.getElementById("classLength").value = "";
+  document.getElementById("numHits").value = "";
+  document.getElementById("classType").value = "";
 }
+
+function classLength() {
+  return document.getElementById("classLength").value;
+}
+
+function numHits() {
+  return document.getElementById("numHits").value;
+}
+
+function classType() {
+  return document.getElementById("classType").value;
+}
+
+function calcCredits(classLength, numHits, classType) {
+  var maxHits = classLength * 4;
+  var perfectNumber = classLength * 3;
+  var grade = numHits * 1.0 / perfectNumber;
+  if (grade > 1.0) {
+    grade = 1.0
+  }
+
+  if (classType == "allOrNothing") {
+    if (grade >= 1) {
+      return classLength;
+    } else {
+      return 0;
+    }
+  }
+
+  if (classType == "fiftyMinuteHours") {
+    classLength = classLength * 50.0/60;
+    classLength = Math.round(classLength * 2)/2;
+    classType = "roundDown"
+  }
+
+  if (classType == "roundDown") {
+    var proRated = grade * classLength;
+    var numCredits = Math.round(proRated * 2)/2;
+    return numCredits;
+  }
+}
+
+function showCredits() {
+  document.getElementById("response").innerHTML = calcCredits(classLength(), numHits(), classType());
+}
+
+
+module.exports = {
+  calcCredits: function(classLength, numHits, classType) {
+    return calcCredits(classLength, numHits, classType);
+  }
+};
---
+++ test/request_test.js
@@ -0,0 +1,48 @@
+var request = require('../request.js');
+var chai = require('chai');
+var expect = chai.expect;
+var solution = require('../solution.js')
+// describe("calcCredits(classLength, numHits, classType)", function() {
+//   describe("allOrNothing", function() {
+//     it("passes a 10/12", function() {
+//       expect(request.calcCredits(3, 10, "allOrNothing")).to.equal(3)
+//     })
+//     it("passes a 9/12", function() {
+//       expect(request.calcCredits(3, 9, "allOrNothing")).to.equal(3)
+//     })
+//     it("fails a 8/12", function() {
+//       expect(request.calcCredits(3, 8, "allOrNothing")).to.equal(0)
+//     })
+//   })
+//   describe("roundDown", function() {
+//     it("gives an 11/28 a 3.5", function() {
+//       expect(request.calcCredits(7, 11, "roundDown")).to.equal(3.5)
+//     })
+//   })
+// })
+function runTest(classLength, numHits, classType, result) {
+  describe("auto " + classType, function() {
+    it("gives a " + numHits + "/" + (classLength * 4) + " a " + result + ", " + classLength + " hour class", function() {
+      expect(request.calcCredits(classLength, numHits, classType)).to.equal(result)
+    })
+  })
+}
+
+var classLengths = [1,2,3,4,5,6,7,8,9,10,11,12]
+
+// console.log(request.calcCredits(12, 48, "allOrNothing"));
+classLengths.forEach(function(classLength) {
+  [
+    "allOrNothing",
+    // "roundDown",
+    // "fiftyMinuteHours"
+  ].forEach(function(classType) {
+  var maxHits = classLength * 4;
+    for (var numHits = 0; numHits <= maxHits; numHits++) {
+      var result = solutions[classType][classLength][numHits];
+      runTest(classLength, numHits, classType, result)
+    }
+  })
+})
+
+// runTest(7, 11, "roundDown", 3.5);
