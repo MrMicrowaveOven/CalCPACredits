db6f996a50d95a5fe6e0dd6adc22f98d013a86b2
Benjamin Zagorski <benj@trustsherpa.com>
2017-03-22 11:12:22 -0700

    Updated algorithm to pass new logic and class lengths

--- calculations.js
+++ calculations.js
@@ -1,11 +1,16 @@
 function calcCredits(classLength, numHits, classType) {
   var maxHits = classLength * 4;
-  var perfectNumber = classLength * 3;
+  // perfectNumber is what a student needs to get to achieve a perfect score.
+  var perfectNumber = Math.floor(maxHits * 3 / 4);
+  // grade is the percentage of hits they got out of the perfectNumber.
   var grade = numHits * 1.0 / perfectNumber;
   if (grade > 1.0) {
     grade = 1.0
   }
-  if (classType == "allOrNothing" || (classLength == 1 && classType == "roundDown")) {
+
+// If it's all or nothing, they either have perfect (grade == 1) or nothing.
+// 1-hour classes are always considered allOrNothing
+  if (classType == "allOrNothing" || (classLength == 1)) {
     if (grade >= 1) {
       return classLength;
     } else {
@@ -13,20 +18,22 @@ function calcCredits(classLength, numHits, classType) {
     }
   }
 
-  if (classType == "fiftyMinuteHours") {
+// For fiftyMinuteHours, I simply multiply the classLength by 50/60,
+//  then round to the nearest .5 hour.
+  if (classType == "fiftyMinuteHours"  && classLength > 1) {
     classLength = classLength * 50.0/60;
     classLength = Math.floor(classLength * 2)/2;
-    classType = "roundDown"
   }
 
-  if (classType == "roundDown") {
-    var proRated = grade * classLength;
-    var numCredits = Math.floor(proRated * 2)/2;
-    if (numCredits < 1) {
-      numCredits = 0;
-    }
-    return numCredits;
+  // The number of credits the student has earned, before rounding
+  var proRated = grade * classLength;
+  // The number of credits, rounded down to .5 hour.
+  var numCredits = Math.floor(proRated * 2)/2;
+  // roundDown has a minimum of 1 credit.  If it's less, round down to 0.
+  if (numCredits < 1 && classType == "roundDown") {
+    numCredits = 0;
   }
+  return numCredits;
 }
 
 function updateHits() {
@@ -47,6 +54,7 @@ function updateHits() {
     hitsDropdown.appendChild(option);
   }
 }
+
 function classLength() {
   return document.getElementById("classLength").value;
 }
@@ -58,7 +66,6 @@ function numHits() {
 function classType() {
   return document.getElementById("classType").value;
 }
-
 
 function showCredits() {
   var numCredits = calcCredits(classLength(), numHits(), classType());
